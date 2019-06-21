8488ac20f3f88f6c922d0c1bdda9343566a85deb
MICHAEL DUVAL <duv74@hotmail.com>
2019-06-21 18:49:06 -0400

    more changes

--- PyBank.py
+++ PyBank.py
@@ -1,11 +1,36 @@
 import os
-
 import csv
 
-csvpath = os.path.join('python_challenge', 'budget_data.csv')
+# PRINT INTRODUCTIO TO ANALYSIS
 
-with open(csvpath, newline='') as csvfile:
+print("Financial Analysis")
+
+print("--------------------------")
+
+# TASK ONE - CALCULATE TOTAL MONTHS IN DATASET
+
+budget_csv = os.path.join('..', "python-challenge", 'budget_data.csv')
+
+with open(budget_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+    
+    data = list(csvreader)
+    
+    row_count = len(data)
+    
+    print("Total Months: " + str(row_count - 1))
 
-    print(csvreader)
+# TASK TWO - CALCULATE NET TOTAL P/L ENTIRE PERIOD
 
+    net_total = 0
+
+    for row in csv.reader(csvfile):
+        for col in row[1]:
+            net_total += int(col)
+    print("Total: $" + str(net_total))
+
+# TASK THREE - CALCULATE AVERAGE P/L
+
+# TASK FOUR - CALCULATE GREATEST INCREASE IN PROFIT
+
+# TASK FIVE - CALCULATE GREATEST DECREASE IN LOSS
